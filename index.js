fc53c7392e8d30686cb8f01ce60cc4a25318c991
epicnaud <vererfvea@gmail.com>
2026-03-01 17:53:47 +0100

    Initialize mineflayer bot and set up event listeners

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-{ "dependencies": { "mineflayer": "latest" } }
+const mineflayer = require('mineflayer')
+const bot = mineflayer.createBot({
+    host: 'JOUW_SERVER.aternos.me', 
+    username: 'Botje247'
+})
+bot.on('login', () => console.log('Ik ben online!'))
+bot.on('end', () => setTimeout(() => location.reload(), 5000))
